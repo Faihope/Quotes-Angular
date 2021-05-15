8d668ddaf3e3f44c388deadadc14356702b78594
Fai <chepngetichfaith46@gmail.com>
2021-05-16 00:30:16 +0300

    add the functionality to the delete function

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -15,12 +15,23 @@ export class QuotesComponent implements OnInit {
   showDetails(index){
     this.quote[index].showQuotes = !this.quote[index].showQuotes;
   }
+  
+  deleteQuote(isRead, index){
+    
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}?`)
+
+      if (toDelete){
+        this.quote.splice(index,1)
+      }
+    
+  }}
   addNewQuote(quotes){
     let quotesLength = this.quote.length;
     quotes.id = quotesLength+1;
     quotes.completeDate = new Date(quotes.completeDate)
     this.quote.push(quotes)
-  }
+  };
 
   constructor() { }
 
